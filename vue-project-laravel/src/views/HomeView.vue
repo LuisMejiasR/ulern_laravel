d83d764e60128ef6b9659440aee0b51e56325329
LuisMejias <luisme.1993@gmail.com>
2025-01-17 18:25:01 -0300

    feat: cleaning project, adding tailwind css & vite configs

--- vue-project-laravel/src/views/HomeView.vue
+++ vue-project-laravel/src/views/HomeView.vue
@@ -1,9 +1,9 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+
 
 <template>
   <main>
-    <TheWelcome />
+    <h1 class="title">
+      Mi perfil
+    </h1>
   </main>
 </template>
